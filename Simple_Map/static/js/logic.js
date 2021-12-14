d5757df8aadd713edb2fe9f6d3c842eccdc72afc
Kayleigh Bradley-Vogt <kbradleyvogt@gmail.com>
2021-12-13 19:10:13 -0600

    Addressing Conflict

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -2,17 +2,15 @@
 console.log("working");
 
 // Create the map object with a center and zoom level.
-let map = L.map("mapid").setView([34.0522,-118.2437], 14);
-
-//  Add a marker to the map for Los Angeles, California.
-L.circle([34.0522, -118.2437], {
-    color: 'black',
-    fillColor: '#4f077'
-    radius: 300
- }).addTo(map);
+let map = L.map("mapid", {
+    center: [
+      40.7, -94.5
+    ],
+    zoom: 4
+  });
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
