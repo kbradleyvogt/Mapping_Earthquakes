0cb0b01e1d8f712555ced1eed59529bf860cc64f
Kayleigh Bradley-Vogt <kbradleyvogt@gmail.com>
2021-12-18 13:21:11 -0600

    Update

--- Mapping_GeoJSON_Polygons/static/js/logic.js
+++ Mapping_GeoJSON_Polygons/static/js/logic.js
@@ -30,3 +30,14 @@ let map = L.map('mapid', {
 
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
+
+// Accessing the Toronto neighborhoods GeoJSON URL.
+let torontoHoods = "https://raw.githubusercontent.com/kbradleyvogt/Mapping_Earthquakes/main/torontoNeighborhoods.json";
+
+// Grabbing our GeoJSON data.
+d3.json(torontoHoods).then(function(data){
+    console.log(data);
+    //Creating a GeoJSON layer wit h the retrieved data.
+    L.geoJSON(data).addTo(map);
+});
+
